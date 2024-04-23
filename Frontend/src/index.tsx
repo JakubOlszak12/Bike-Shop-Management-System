8a2cd1d2048c812c614bb3be4178eb1f45eb2950
jakubolszak12 <j.jakub66@gmail.com>
2024-04-23 14:57:05 +0200

    implemented login/logout

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -1,14 +1,50 @@
 /* istanbul ignore file */
 import "~/styles/index.scss";
-import { StrictMode } from "react";
+import { StrictMode, useEffect, useState } from "react";
 import reactDOMClient from "react-dom/client";
 import { App } from "~/components/App";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { NavBar } from "./components/Navbar";
+import { Login } from "./components/Login";
 const rootContainer = document.createElement("div");
 document.body.appendChild(rootContainer);
 const root = reactDOMClient.createRoot(rootContainer);
+
+const isAuthenticatedFromCookie = () => {
+    const token = document.cookie.split('; ').find(row => row.startsWith('token='));
+    return !!token; // Convert to boolean
+};
+
+const AppContainer = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(isAuthenticatedFromCookie());
+
+    useEffect(() => {
+        setIsAuthenticated(isAuthenticatedFromCookie());
+    }, []);
+
+
+    return (
+        <React.StrictMode>
+            <BrowserRouter>
+            <NavBar isAuthenticated={isAuthenticated} />
+             <Routes>
+                <Route path="/login" element={<Login />}/>
+                <Route path="/clients" element={
+                    <ProtectedRoute>
+                        <App />
+                    </ProtectedRoute>
+                    } />
+             </Routes>
+            </BrowserRouter>
+        </React.StrictMode>
+    );
+
+};
+
 root.render(
-    <StrictMode>
-        <App />
-    </StrictMode>
+    <AppContainer />
 );
+
+
